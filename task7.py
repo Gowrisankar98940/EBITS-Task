f16da10b8f753a8eb03070ff0acef7548bdb4a7d
Gowrisankar <sankarsrini98940@gmail.com>
2019-06-05 15:01:45 +0530

    05062019 8

---
+++ task7.py
@@ -0,0 +1,16 @@
+#### 0 1 1 2 3 5 8 13
+
+
+a=0
+b=1
+i=0
+n=int(input("Enter the number:"))
+while(i < n):
+    if i <= 1:
+        c = i
+    else:
+        c=a+b
+        a=b
+        b=c
+    i=i+1
+    print('serious are:',c)
