ef507b02b4567f251910f1d130f84803b0d44db5
Gowrisankar <sankarsrini98940@gmail.com>
2019-06-05 10:37:57 +0530

    05062019 4

--- task2.py
+++ task2.py
@@ -1,12 +1,13 @@
 a=int(input("Enter the number:"))
 b=int(input("enter the divide num:"))
 if(a%4==0):
-  print('divide by four')
+    print('divide by four')
 elif(a%2==0):
-  print('even num')
+    print('even num')
 else:
-  print('ood num')
+    print('ood num')
 if(a%b==0):
-  print(a,"divides evenly by",b)
+    print(a,"divides evenly by",b)
 else:
-  print(a,"not divides evenly by",b)
+    print(a,"not divides evenly by",b)
+
