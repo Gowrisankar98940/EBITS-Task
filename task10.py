ef27445bcf8a2c2e5def2c9c489421216333041b
Gowrisankar <sankarsrini98940@gmail.com>
2019-06-06 14:59:34 +0530

    06062019 11

---
+++ task10.py
@@ -0,0 +1,10 @@
+a= input("Enter the name:")
+if(a== 'abc'):
+    print('Enter the password')
+    b= input("Enter the password:")
+    if(b== '123'):
+        print('Login successfully')
+    else:
+        print('Password incorrect')
+else:
+    print('User name is incorrect')    
