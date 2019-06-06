88eea18b888cd50545d456b6849922b47bfa213d
Gowrisankar <sankarsrini98940@gmail.com>
2019-06-06 14:19:44 +0530

    06062019 10

--- task8.py
+++ task8.py
@@ -1,7 +1,7 @@
-user1 = input("What's your name?")
-user2 = input("And your name?")
-user1_answer = input("%s, do yo want to choose rock, paper or scissors?", user1)
-user2_answer = input("%s, do you want to choose rock, paper or scissors?", user2)
+user1 = input("What's your name:")
+user2 = input("And your name:")
+user1_answer = input("%s, do yo want to choose rock, paper or scissors:" % user1)
+user2_answer = input("%s, do you want to choose rock, paper or scissors:" % user2)
 
 def compare(u1, u2):
     if u1 == u2:
---
+++ task9.py
@@ -0,0 +1,9 @@
+a=int(input("Enter the mark:"))
+if(a>=35 and a<=45):
+    print("pass")
+elif(a>=45 and a<=60):
+    print("B grade")
+elif(a>=60 and a<=80):
+    print("A grade")
+else:
+    print("Fail")
