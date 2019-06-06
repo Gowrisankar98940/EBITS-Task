4ab0c2fed12aee135a1875886429ce0209ecdace
Gowrisankar <sankarsrini98940@gmail.com>
2019-06-06 16:53:26 +0530

    06062019 13

--- task12.py
+++ task12.py
@@ -1,12 +1,11 @@
-
 n = int(input("enter the rows:"))
 i = 0
 h = "--- "
 v = "|   "
 h = h * n
-v = v * (n+1)
+v = v*(n+1)
 while (i < n+1):
-  print (h)
-  if(i != n):
-    print (v)
-  i += 1
+    print (h)
+    if(i != n):
+        print (v)
+    i+=1
